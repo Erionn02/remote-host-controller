03b11e5bdf5df9d034e02204a1a5a8681bee0fec
Jakub Jabłoński <jkjablonski@gmail.com>
2023-03-13 00:25:39 +0100

    messing around with AES encryption

--- tests/integration_tests/SecureSocketTest.cpp
+++ tests/integration_tests/SecureSocketTest.cpp
@@ -2,7 +2,11 @@
 
 #include "auxiliary/socket/SecureSocket.hpp"
 
-
+#include <cryptopp/rsa.h>
+#include <cryptopp/rng.h>
+#include <cryptopp/randpool.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/modes.h>
 
 
 TEST(SecureSocketTest, IsMessageCiphered){
@@ -15,5 +19,87 @@ TEST(SecureSocketTest, IsMessageCiphered){
 
     auto data = socket.getsockopt<int>(ZMQ_TYPE);
 
-    std::cout<<data<<" "<<ZMQ_PULL<<" "<<ZMQ_PUSH;
+    std::cout<<data<<" "<<ZMQ_REP<<" "<<ZMQ_REQ;
+
+    CryptoPP::AutoSeededRandomPool rng;
+    CryptoPP::InvertibleRSAFunction params;
+    params.GenerateRandomWithKeySize(rng,2048);
+    const CryptoPP::Integer& n = params.GetModulus();
+    const CryptoPP::Integer& p = params.GetPrime1();
+    const CryptoPP::Integer& q = params.GetPrime2();
+    const CryptoPP::Integer& d = params.GetPrivateExponent();
+    const CryptoPP::Integer& e = params.GetPublicExponent();
+    using namespace std;
+
+
+    cout << "RSA Parameters:" << endl;
+    cout << " n: " << n << endl;
+    cout << " p: " << p << endl;
+    cout << " q: " << q << endl;
+    cout << " d: " << d << endl;
+    cout << " e: " << e << endl;
+    cout << endl;
+
+    //Key and IV setup
+    //AES encryption uses a secret key of a variable length (128-bit, 196-bit or 256-
+    //bit). This key is secretly exchanged between two parties before communication
+    //begins. DEFAULT_KEYLENGTH= 16 bytes
+    const int KEY_SIZE=16;
+    CryptoPP::byte key[ KEY_SIZE ], iv[ CryptoPP::AES::BLOCKSIZE ];
+    memset( key, 0x00, KEY_SIZE );
+    memset( iv, 0x00, CryptoPP::AES::BLOCKSIZE );
+
+    //
+    // String and Sink setup
+    //
+    std::string plaintext = "Now is the time for all good men to come to the aide...";
+    std::string ciphertext;
+    std::string decryptedtext;
+
+    //
+    // Dump Plain Text
+    //
+    std::cout << "Plain Text (" << plaintext.size() << " bytes)\n" << plaintext<<'\n';
+
+    //
+    // Create Cipher Text
+    //
+    CryptoPP::AES::Encryption aesEncryption(key, KEY_SIZE);
+    CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption( aesEncryption, iv );
+
+    CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink( ciphertext ) );
+    stfEncryptor.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() );
+    stfEncryptor.MessageEnd();
+
+    //
+    // Dump Cipher Text
+    //
+    std::cout << "Cipher Text (" << ciphertext.size() << " bytes)" << std::endl;
+
+    for( int i = 0; i < ciphertext.size(); i++ ) {
+
+        std::cout << "0x" << std::hex << (0xFF & static_cast<CryptoPP::byte>(ciphertext[i])) << " ";
+    }
+
+    std::cout << std::endl << std::endl;
+
+    //
+    // Decrypt
+    //
+    CryptoPP::AES::Decryption aesDecryption(key, KEY_SIZE);
+    CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption( aesDecryption, iv );
+
+    CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink( decryptedtext ) );
+    stfDecryptor.Put( reinterpret_cast<const unsigned char*>( ciphertext.c_str() ), ciphertext.size() );
+    stfDecryptor.MessageEnd();
+
+    //
+    // Dump Decrypted Text
+    //
+    std::cout << "Decrypted Text: " << std::endl;
+    std::cout << decryptedtext;
+    std::cout << std::endl << std::endl;
+
+
+
 }
