0555a8cea502d0810ac69ff3f8c3d7e75689e356
Jakub Jabłoński <jkjablonski@gmail.com>
2023-03-20 23:47:15 +0100

    adding AES and RSA classes

---
+++ src/auxiliary/RSACryptographer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kuba on 20.03.23.
+//
+
+#include "auxiliary/RSACryptographer.hpp"
--- tests/integration_tests/SecureSocketTest.cpp
+++ tests/integration_tests/SecureSocketTest.cpp
@@ -9,26 +9,15 @@
 #include <cryptopp/modes.h>
 
 
-TEST(SecureSocketTest, IsMessageCiphered){
-
-//    SecureSocket socket_server{std::make_unique<ZMQSocket>(zmq::socket_type::rep)};
-//    SecureSocket socket_client{std::make_unique<ZMQSocket>(zmq::socket_type::req)};
-
-    zmq::context_t context;
-    zmq::socket_t socket{context, zmq::socket_type::push};
-
-    auto data = socket.getsockopt<int>(ZMQ_TYPE);
-
-    std::cout<<data<<" "<<ZMQ_REP<<" "<<ZMQ_REQ;
-
+TEST(SecureSocketTest, IsMessageCiphered) {
     CryptoPP::AutoSeededRandomPool rng;
     CryptoPP::InvertibleRSAFunction params;
-    params.GenerateRandomWithKeySize(rng,2048);
-    const CryptoPP::Integer& n = params.GetModulus();
-    const CryptoPP::Integer& p = params.GetPrime1();
-    const CryptoPP::Integer& q = params.GetPrime2();
-    const CryptoPP::Integer& d = params.GetPrivateExponent();
-    const CryptoPP::Integer& e = params.GetPublicExponent();
+    params.GenerateRandomWithKeySize(rng, 2048);
+    const CryptoPP::Integer &n = params.GetModulus();
+    const CryptoPP::Integer &p = params.GetPrime1();
+    const CryptoPP::Integer &q = params.GetPrime2();
+    const CryptoPP::Integer &d = params.GetPrivateExponent();
+    const CryptoPP::Integer &e = params.GetPublicExponent();
     using namespace std;
 
 
@@ -44,88 +33,61 @@ TEST(SecureSocketTest, IsMessageCiphered){
     //AES encryption uses a secret key of a variable length (128-bit, 196-bit or 256-
     //bit). This key is secretly exchanged between two parties before communication
     //begins. DEFAULT_KEYLENGTH= 16 bytes
-    const int KEY_SIZE=16;
-    CryptoPP::byte key[ KEY_SIZE ], iv[ CryptoPP::AES::BLOCKSIZE ];
-    memset( key, 0x00, KEY_SIZE );
-    memset( iv, 0x00, CryptoPP::AES::BLOCKSIZE );
+    const int KEY_SIZE = 16;
+    CryptoPP::byte key[KEY_SIZE], iv[CryptoPP::AES::BLOCKSIZE];
+    memset(key, 0x00, KEY_SIZE);
+    memset(iv, 0x00, CryptoPP::AES::BLOCKSIZE);
 
     //
     // String and Sink setup
     //
-    std::string plaintext = "Now is the time for all good men to come to the aide...";
+    std::string plaintext;
+    std::size_t size = 10'000'000;
+    plaintext.resize(size);
+    auto generator = []() {
+        return std::rand() % 255 + 1;
+    };
+    std::generate_n(plaintext.data(), size, generator);
     std::string ciphertext;
     std::string decryptedtext;
 
     //
     // Dump Plain Text
     //
-    std::cout << "Plain Text (" << plaintext.size() << " bytes)\n" << plaintext<<'\n';
 
     //
     // Create Cipher Text
     //
     CryptoPP::AES::Encryption aesEncryption(key, KEY_SIZE);
-    CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption( aesEncryption, iv );
+    CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption(aesEncryption, iv);
 
-    CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink( ciphertext ) );
-    stfEncryptor.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() );
+    CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink(ciphertext));
+    stfEncryptor.Put(reinterpret_cast<const unsigned char *>( plaintext.c_str()), plaintext.length());
     stfEncryptor.MessageEnd();
 
     //
     // Dump Cipher Text
     //
-    std::cout << "Cipher Text (" << ciphertext.size() << " bytes)" << std::endl;
-
-    for( int i = 0; i < ciphertext.size(); i++ ) {
-
-        std::cout << "0x" << std::hex << (0xFF & static_cast<CryptoPP::byte>(ciphertext[i])) << " ";
-    }
-
-    std::cout << std::endl << std::endl;
+//    std::cout << "Cipher Text (" << ciphertext.size() << " bytes)" << std::endl;
+//
+//    for( int i = 0; i < ciphertext.size(); i++ ) {
+//
+//        std::cout << "0x" << std::hex << (0xFF & static_cast<CryptoPP::byte>(ciphertext[i])) << " ";
+//    }
+//
+//    std::cout << std::endl << std::endl;
 
     //
     // Decrypt
     //
     CryptoPP::AES::Decryption aesDecryption(key, KEY_SIZE);
-    CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption( aesDecryption, iv );
+    CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption(aesDecryption, iv);
 
-    CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink( decryptedtext ) );
-    stfDecryptor.Put( reinterpret_cast<const unsigned char*>( ciphertext.c_str() ), ciphertext.size() );
+    CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink(decryptedtext));
+    stfDecryptor.Put(reinterpret_cast<const unsigned char *>( ciphertext.c_str()), ciphertext.size());
     stfDecryptor.MessageEnd();
 
-    //
-    // Dump Decrypted Text
-    //
-    std::cout << "Decrypted Text: " << std::endl;
-    std::cout << decryptedtext;
-    std::cout << std::endl << std::endl;
-
-
-
-}
-
-
-TEST(SecureSocketTest, testingLimitationsOfZMQ){
-    srand(time(nullptr));
-
-    ZMQSocket client{zmq::socket_type::req};
-    ZMQSocket server{zmq::socket_type::rep};
-    std::string bind_address = "tcp://127.0.0.1:2137";
-
-    server.bind(bind_address);
-    client.connect(bind_address);
-    std::size_t data_size = 1'000'000;
-    auto data = std::make_unique<unsigned char[]>(data_size);
-    std::generate_n(data.get(),data_size,generator);
-
-    zmq::message_t message{data.get(), data_size};
-
-    client.send(std::move(message));
-
-    zmq::message_t received{1};
-    server.recv(received);
-
-
-    ASSERT_EQ(received.size(), data_size);
-    ASSERT_EQ(std::memcmp(received.data(), data.get(), data_size),0);
+    std::cout<<decryptedtext.length();
+    ASSERT_EQ(plaintext.length(), decryptedtext.length());
+    ASSERT_EQ(plaintext, decryptedtext);
 }
