61c26d1a848497a5442a73983a324f47fb12dd8b
Jakub Jabłoński <jkjablonski@gmail.com>
2023-04-06 18:44:24 +0200

    added comment in test

--- tests/unit_tests/AESCryptographerTest.cpp
+++ tests/unit_tests/AESCryptographerTest.cpp
@@ -26,11 +26,11 @@ TEST_F(AESCryptographerTest, canDecipherCipheredData) {
     std::string some_random_data = generateRandomData();
     AESCryptographer cryptographer{};
 
-    //when
+    // when
     auto ciphered_data = cryptographer.cipherData(some_random_data);
     auto deciphered_data = cryptographer.decipherData(ciphered_data);
 
-    //then
+    // then
     ASSERT_GE(ciphered_data.length(), some_random_data.length());
     ASSERT_EQ(deciphered_data, some_random_data);
 }
@@ -42,11 +42,11 @@ TEST_F(AESCryptographerTest, canDecipherDataMultipleTimes) {
     for (int i = 0; i < 10; ++i) {
         auto some_random_data = generateRandomData();
 
-        //when
+        // when
         auto ciphered_data = cryptographer.cipherData(some_random_data);
         auto deciphered_data = cryptographer.decipherData(ciphered_data);
 
-        //then
+        // then
         ASSERT_EQ(deciphered_data, some_random_data);
     }
 
@@ -58,10 +58,13 @@ TEST_F(AESCryptographerTest, oneCryptographerCanDecipherCipheredDataByOther) {
     AESCryptographer cryptographer_1{};
     AESCryptographer cryptographer_2{cryptographer_1.getKey(), cryptographer_1.getInitVector()};
 
-    //when
+    // when
     auto ciphered_data_1 = cryptographer_1.cipherData(some_random_data.data(), some_random_data.size());
     auto ciphered_data_2 = cryptographer_2.cipherData(some_random_data.data(), some_random_data.size());
 
+
+    // then
+    // That test might seem like overkill, but believe me, it is not. It caught bug.
     ASSERT_EQ(ciphered_data_1, ciphered_data_2);
     auto deciphered_1 = cryptographer_1.decipherData(ciphered_data_2);
     auto deciphered_1_again = cryptographer_1.decipherData(ciphered_data_2);
@@ -79,11 +82,11 @@ TEST_F(AESCryptographerTest, differentCryptographersWillGenerateDifferentCiphere
     AESCryptographer some_cryptographer{};
     AESCryptographer some_other_cryptographer{};
 
-    //when
+    // when
     auto ciphered_data_first = some_cryptographer.cipherData(some_random_data);
     auto ciphered_data_second = some_other_cryptographer.cipherData(some_random_data);
 
 
-    //then
+    // then
     ASSERT_NE(std::memcmp(ciphered_data_first.data(), ciphered_data_second.data(), data_length), 0);
 }
