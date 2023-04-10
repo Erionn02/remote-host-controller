1cd8fe96dc0c321855793567c0c094bfe5e0e109
Jakub Jabłoński <jkjablonski@gmail.com>
2023-04-10 10:08:01 +0200

    downloaded third party pstreams header

--- include/RemoteHostConnectionController.hpp
+++ include/RemoteHostConnectionController.hpp
@@ -3,6 +3,8 @@
 #include "auxiliary/socket/ISocket.hpp"
 #include "auxiliary/Runnable.hpp"
 
+#include "auxiliary/THIRD_PARTY/pstream.hpp"
+
 class RemoteHostConnectionController : public Runnable {
 public:
     explicit RemoteHostConnectionController(std::unique_ptr<ISocket> socket, const std::string &address_without_port);
