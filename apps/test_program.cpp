3b2c938f4323228a20cafcdc882f8839b6d55001
Jakub Jabłoński <jkjablonski@gmail.com>
2023-04-19 00:31:10 +0200

    went back to old version of testprogram

--- apps/test_program.cpp
+++ apps/test_program.cpp
@@ -6,13 +6,22 @@
 using namespace std::chrono_literals;
 
 int main() {
-    std::cout<<"Started program.";
+    std::cout<<"Started program.\n";
     while(true){
-        std::cout<<"\nEnter input: ";
-        std::string lol{};
+        std::cout<<"some input lmaoo\n";
+        std::cerr<<"\nsomething in error stream\n";
+        if(std::rand()%3==0){
+            std::cout<<"\nEnter input: ";
+            std::string lol{};
 
-        std::getline(std::cin, lol);
-        std::cout<<'\b';
+            std::getline(std::cin, lol);
+            std::cout<<"Got input: "<<lol<<'\n';
+            if(lol == "end"){
+                std::cout<<"Terminating.";
+                break;
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
     return 0;
 }
